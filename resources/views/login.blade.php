3580c50a57c5cfd384dea7fed00ba8ea7c05a871
fadlyian <mansyuraw24@gmail.com>
2023-02-16 18:38:15 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Page</title>
+</head>
+<body>
+    <h1>Halaman Login</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius culpa quod aut magnam assumenda nisi doloribus. Consequatur ex suscipit beatae provident tempora quos repudiandae exercitationem reprehenderit earum saepe velit repellendus ratione natus deleniti in veritatis, ducimus nihil neque, dolor quis optio. Error velit saepe natus distinctio sequi maiores necessitatibus odio sapiente quisquam magnam iusto voluptatem quis ducimus quidem unde veniam, quia, ad dolorem maxime, nobis repellat temporibus quod soluta aliquid! Eaque quod fuga adipisci aut obcaecati, tempore optio est labore incidunt autem illum voluptas dignissimos assumenda sit voluptates distinctio voluptatum. Sunt dolorem eveniet doloribus excepturi veniam minima laborum corrupti soluta!</p>
+</body>
+</html>
